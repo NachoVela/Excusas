5b035941f1bbe29403a8884d2c9b399020e0bcb3
Nacho <ignaciovelamd@gmail.com>
2025-10-21 15:13:14 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['Tu madre', 'Mi primo', 'El presidente', 'Dios'];
+  let action = [' se ha comido', ' ha destrozado por completo', ' ha vendido', ' ha esclavizado'];
+  let what = [' a mi perro', ' mi coche', ' a la humanidad', ' el continuo espacio-tiempo'];
+  let when = [' justo antes de venir', ' justo ahora', ' cuando salía de casa', ' mientras dormía'];
+
+  let elementExcuse = this.document.getElementById('excusa');
+
+  let excuseContent = who[Math.floor(Math.random() * 4)] 
+  + action[Math.floor(Math.random() * 4)] 
+  + what[Math.floor(Math.random() * 4)]
+  + when[Math.floor(Math.random() * 4)];
+
+  elementExcuse.innerHTML = excuseContent;
+
 };
